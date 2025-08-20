145561a012eb715ba59a84c1df626b0cf8954c51
jonathanckraus <163678222+jonathanckraus@users.noreply.github.com>
2025-08-20 14:48:02 -0400

    Id

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -8,7 +8,7 @@ import prisma from '../../lib/prisma';
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
     where: {
-      id: String(params?.authorId),
+      id: String(params?.id),
     },
     include: {
 
