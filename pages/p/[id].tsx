34d0143064e75a844f3ead94bcf5e4bf42770249
jonathanckraus <163678222+jonathanckraus@users.noreply.github.com>
2025-08-20 14:35:13 -0400

    unique

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -6,7 +6,7 @@ import { PostProps } from "../../components/Post"
 import prisma from '../../lib/prisma';
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const post = await prisma.post.findMany({
+  const post = await prisma.post.findUnique({
     where: {
       id: String(params?.id),
     },
