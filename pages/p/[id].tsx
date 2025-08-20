7522773b9a4427a81b4dc5931c5127e3d7161e23
jonathanckraus <163678222+jonathanckraus@users.noreply.github.com>
2025-08-20 15:24:57 -0400

    j2

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -35,6 +35,7 @@ const Post: React.FC<PostProps> = (props) => {
       <div>
         <h2>{title}</h2>
         <p>By {props?.author?.name || "Unknown author"}</p>
+        <p>Date {props?.createdj || "Unknown date"}</p>
         <ReactMarkdown children={props.content} />
       </div>
       
