980a5fbcb4630ea2135f2c59836c47cd6efac8aa
jonathanckraus <163678222+jonathanckraus@users.noreply.github.com>
2025-08-20 14:24:15 -0400

    Change name to id

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -13,7 +13,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     include: {
 
       author: {
-        select: { name: true },
+        select: { id: true },
       },
   },
   });
